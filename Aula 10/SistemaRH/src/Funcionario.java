d33769437c6c354712700713cd1e101a26b4fb09
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2024-07-22 20:47:07 -0300

    Criação Aula 10

---
+++ Aula 10/SistemaRH/src/Funcionario.java
@@ -0,0 +1,27 @@
+public class Funcionario {
+    public int matricula;
+    public String nome;
+    public String cargo;
+    public String cpf;
+    public double salario;
+
+    public Funcionario(int matricula, String nome, String cargo, String cpf, double salario) {
+
+        this.matricula = matricula;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public void mostrarInformacoes(){
+        System.out.printf("----------Funcionario %d----------\n", this.matricula);
+
+        System.out.println("Matricula: "+this.matricula);
+        System.out.println("Nome: "+this.nome);
+        System.out.println("CPF: "+this.cpf);
+        System.out.println("Cargo: "+this.cargo);
+        System.out.printf("Salario: R$ %.2f \n",this.salario);
+    }
+
+}
